ba916bbbceeea3d5afd992620f6dad2d0efb0aab
Sparta <spartaofomg@gmail.com>
2024-12-30 10:59:54 -0500

    241230 - fixing handing 0x0d (CR out) in GIF creation, fixing unused dir entry count in GIF and PNG outputs

--- dart.cpp
+++ dart.cpp
@@ -705,9 +705,12 @@ void OutputCR()
     {
         CursorY++;
     }
-    OutputToScreen(0x20);
-    CharX = 0;
-    CursorX = 0;
+    if (((size_t)(CharY * 80) >= ScrRam.size()) || (ScrRam[(CharY * 80) + CharX] == 0x00))
+    {
+        OutputToScreen(0x20);
+        CharX = 0;
+        CursorX = 0;
+    }
     
     return;
 }
@@ -1590,7 +1593,9 @@ bool ConvertD64ToGif()
     //DirSector = 1;
     //DirPos = 2;
 
-    while (DirPos != 0)
+    bool Finished = false;
+
+    while (!Finished)
     {
         if (Disk[Track[DirTrack] + (DirSector * 256) + DirPos] != 0)
         {
@@ -1710,15 +1715,25 @@ bool ConvertD64ToGif()
             DirPos -= 256;
             int NT = Disk[Track[DirTrack] + (DirSector * 256) + 0];
             int NS = Disk[Track[DirTrack] + (DirSector * 256) + 1];
-            DirTrack = NT;
-            DirSector = NS;
 
-            if (DirTrack == 0)
+            if (NT != 0)
             {
-                DirPos = 0;
+                DirTrack = NT;
+                DirSector = NS;
+            }
+            else
+            {
+                Finished = true;
             }
         }
-        
+        else
+        {
+            if (Disk[Track[DirTrack] + (DirSector * 256) + DirPos] == 0)
+            {
+                Finished = true;
+            }
+        }
+
         if (CursorY == 24)
         {
             CreateGifFrame();
@@ -1863,7 +1878,9 @@ bool ConvertD64ToPng()
     DirSector = 1;
     DirPos = 2;
 
-    while (DirPos != 0)
+    bool Finished = false;
+
+    while (!Finished)
     {
         if (Disk[Track[DirTrack] + (DirSector * 256) + DirPos] != 0)
         {
@@ -1978,15 +1995,24 @@ bool ConvertD64ToPng()
             DirPos -= 256;
             int NT = Disk[Track[DirTrack] + (DirSector * 256) + 0];
             int NS = Disk[Track[DirTrack] + (DirSector * 256) + 1];
-            DirTrack = NT;
-            DirSector = NS;
-
-            if (DirTrack == 0)
+            
+            if (NT != 0)
             {
-                DirPos = 0;
+                DirTrack = NT;
+                DirSector = NS;
+            }
+            else
+            {
+                Finished = true;
             }
         }
-
+        else
+        {
+            if (Disk[Track[DirTrack] + (DirSector * 256) + DirPos] == 0)
+            {
+                Finished = true;
+            }
+        }
 #ifdef DEBUG
         //CreatePng();
         //ThisDirEntry++;
@@ -2764,7 +2790,7 @@ bool ImportFromD64()
         FixBAM(Disk);       //In case we imported from an old Sparkle disk...
     }
 
-     return true;
+    return true;
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -5007,11 +5033,11 @@ int main(int argc, char* argv[])
     {
 
     #ifdef DEBUG
-        InFileName = "c:/dart/test/asm3.asm";
-        //OutFileName = "c:/dart/lmdf.png";
+        InFileName = "c:/dart/test/anim/tna.d64";
+        OutFileName = "c:/dart/tna.gif";
         //argSkippedEntries = "all";
         argEntryType = "del";
-        //argPalette = "18";
+        argPalette = "18";
     #else
         cout << "Usage: dart input [options]\n";
         cout << "options:    -o <output.d64> or <output.png> or <output.gif>\n";
@@ -5547,12 +5573,16 @@ int main(int argc, char* argv[])
 
     int NumFreeSectors = 0;
 
-    if ((DirTrack == 18) && (DirPos != 0))
+    if (DirTrack == 18)
     {
         NumFreeSectors = Disk[Track[18] + (size_t)(18 * 4)];
-        NumFreeEntries = NumFreeSectors * 8;        for (int i = DirPos + 32; i < 256; i += 32)
+        NumFreeEntries = NumFreeSectors * 8;
+        for (int i = 2; i < 256; i += 32)
         {
-            NumFreeEntries++;
+            if (Disk[Track[DirTrack] + (DirSector * 256) + i] == 0)
+            {
+                NumFreeEntries++;
+            }
         }
     }
 
